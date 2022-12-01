1fbf0d0013d761d7fd8a846b119d95981a55fdec
Korbexmachina <86434527+YoctoMonkey@users.noreply.github.com>
2022-11-30 17:30:32 -0800

    Updated README, started work on main.py, implemented first scenario

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Ethics-and-Environment
+
+Welcome to the team, Big Executive! Your job is to improve the corporation. Good luck!
+
+A game about making decisions and balancing environmental protection with profit. There are 6 different endings depending on the players ability to make the best decision for each of 10 scenarios.
---
+++ main.py
@@ -0,0 +1,11 @@
+from scenario import scenario
+
+money = 50
+environment = 50
+
+scenarios = []
+
+scenario0 = scenario("Oh no! Those pesky environmental regulations are really harshing our company’s mellow. What should we do?", 
+                     "Keep company manufacturing in regulated country (no change in environmental impact, - profit)",
+                     "Move company manufacturing to less regulated country(- - environmental impact, ++ profit)",
+                     
