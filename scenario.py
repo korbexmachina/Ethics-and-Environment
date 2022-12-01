2530f2700999927c1c4d3a1a24f8a95a47763495
Korbexmachina <86434527+YoctoMonkey@users.noreply.github.com>
2022-11-30 17:02:45 -0800

    Added docstrings to scenario.py, also made some minor changes

--- scenario.py
+++ scenario.py
@@ -1,5 +1,14 @@
 class scenario:
     def __init__(self, body, option1, option2, option1_impact, option2_impact) -> None:
+        """Initializes a new scenario
+
+        Args:
+            body (str): The body text for the scenario
+            option1 (str): The first option
+            option2 (str): The second option
+            option1_impact (int): The impact that the first choice will have on the points total
+            option2_impact (int): The impact that the second choice will have on the points total
+        """
         self.body = body
         self.option1 = option1
         self.option2 = option2
@@ -7,12 +16,30 @@ class scenario:
         self.option2_impact = option2_impact
     
     def __str__(self) -> None:
+        """Prints the scenario, along with the options
+        """
         print(self.body)
         print("---")
         print(f"{self.option1} : {self.option2}")
         
     def option1_picked(self, points) -> int:
-        return (points -= self.option1_impact)
+        """Adjusts the total number of points
+
+        Args:
+            points (int): The total number of points
+
+        Returns:
+            int: the new number of points after adding option1_impact to the total
+        """
+        return (points + self.option1_impact)
         
     def option2_picked(self, points) -> int:
-        return (points -= self.option2_impact)
+        """Adjusts the total number of points
+
+        Args:
+            points (int): The total number of points
+
+        Returns:
+            int: the new number of points after adding option2_impact to the total
+        """
+        return (points + self.option2_impact)
