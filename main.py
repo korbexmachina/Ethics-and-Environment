0e8b8925f9a5883c94db1c225a28527c467f6501
Korbexmachina <86434527+YoctoMonkey@users.noreply.github.com>
2022-12-03 18:19:55 -0800

    Final changes (for now)

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ scenario1 = Scenario(
 )
 
 scenario2 = Scenario(
-    "There's been a new innovation in manufacturing technology that can really increase our production, oh slight issue though it might have some environmental consequences.",
+    "There's been a new innovation in manufacturing technology that can really increase our production,\noh slight issue though it might have some environmental consequences.",
     "Go for it",
     "No it is too dangerous",
     -3, 2, 1, 0,
@@ -27,7 +27,7 @@ scenario2 = Scenario(
 )
 
 scenario3 = Scenario(
-    "Our factories in the Eastern District are producing extreme levels of toxic pollutants into the air, so much so that people are getting sick, and even dying! It's unlikely that this sort of pollution will be traced back to us though… ",
+    "Our factories in the Eastern District are producing extreme levels of toxic pollutants into the air,\nso much so that people are getting sick, and even dying!\nIt's unlikely that this sort of pollution will be traced back to us though… ",
     "Make some changes to the factories, better safe than sorry...",
     "Ehhhhh whats the worst that could happen...?",
     2, -2, -1, -2,
@@ -35,7 +35,7 @@ scenario3 = Scenario(
 )
 
 scenario4 = Scenario(
-    "Woah! The government is trying to pass a law that would make your current equipment obsolete. If you donate to my campaign, I might just be able to rally some of my party members to oppose it.",
+    "Woah! The government is trying to pass a law that would make your current equipment obsolete.\nIf you donate to my campaign, I might just be able to rally some of my party members to oppose it.",
     "Sure, sounds like a fair deal!",
     "No dice, pal.",
     0, -2, 1, -2,
@@ -43,7 +43,7 @@ scenario4 = Scenario(
 )
 
 scenario5 = Scenario(
-    "I have a great idea! Since we are based near an ocean why don't we start investing in wind turbines to help power our factories, sure it'll cost a bit of money to build but we'll get much more back eventually.",
+    "I have a great idea! Since we are based near an ocean why don't we start investing in wind turbines to help power our factories,\nsure it'll cost a bit of money to build but we'll get much more back eventually.",
     "Great idea!",
     "This isn't some sort of fantasyland of course that won't work",
     1, 1, -1, -1,
@@ -51,7 +51,7 @@ scenario5 = Scenario(
 )
 
 scenario6 = Scenario(
-    "Hey there, Big Executive! I was wondering if you wanted to become a donor for our Environmental Non-Profit. If you donate, you'd get your brand name all over our event banners and maybe even a shiny plaque in our headquarters. You'd like that wouldn't you?",
+    "Hey there, Big Executive! I was wondering if you wanted to become a donor for our Environmental Non-Profit.\nIf you donate, you'd get your brand name all over our event banners and maybe even a shiny plaque in our headquarters.\nYou'd like that wouldn't you?",
     "Brand publicity AND a shiny plaque? I'm in! Oh, also the environment…yeah that's important.",
     "Get out of here you dirty my office floor",
     2, 1, -1, -1,
@@ -79,6 +79,9 @@ money = 10
 environment = 10
 scenarios = [scenario0, scenario1, scenario2, scenario3, scenario4, scenario5, scenario6, scenario7, scenario8]
 
+print("Welcome to the team, Big Executive! Your job is to improve the corporation. Good luck!\nType 1 or 2 to select an option when prompted\nPress Enter to continue:")
+input()
+print("\n" * 100)
 # The actual game loop
 for i in scenarios:
     print(f"Money: {money}\nEnvironmental impact: {environment}")
@@ -101,15 +104,15 @@ for i in scenarios:
 #Endings
 print(f"Money: {money}\nEnvironmental impact: {environment}")
 if environment < -50:
-    print("Our company is being blamed for environmental degradation, we need somebody to blame. Who better than those average joe consumers! Shame on them for ruining the environment.")
+    print("Our company is being blamed for environmental degradation, we need somebody to blame. Who better than those average joe consumers!\nShame on them for ruining the environment.")
 elif environment >= 10 and money >= 10:
-    print("Wow! We at Corporation Co. are astonished at your ability to both turn a profit and make our operations more sustainable. We didn't know that was possible. Keep up the good work!")
+    print("Wow! We at Corporation Co. are astonished at your ability to both turn a profit and make our operations more sustainable.\nWe didn't know that was possible. Keep up the good work!")
 elif environment <= 10 and money < 10:
     print("Your decisions have cost us financially and the board believes someone more amenable to our goals deserves your position.")
 elif environment < 10 and money < 10:
-    print("Your decisions have cost us financially and the board believes someone more amenable to our goals deserves your position. We now have to file for bankruptcy and deal with an innumerable amount of federal and state lawsuits.")
+    print("Your decisions have cost us financially and the board believes someone more amenable to our goals deserves your position.\nWe now have to file for bankruptcy and deal with an innumerable amount of federal and state lawsuits.")
 elif environment > 10 and money < 10:
-    print("You were so busy worrying about the “environment” and being “sustainable” that you ran Corporation Co. into the ground, thanks to you millions of people are unemployed!")
+    print("You were so busy worrying about the “environment” and being “sustainable” that you ran Corporation Co. into the ground,\nthanks to you millions of people are unemployed!")
 elif environment >= 10 and money > 10:
     print("You did your job well. We made a profit.")
 else:
